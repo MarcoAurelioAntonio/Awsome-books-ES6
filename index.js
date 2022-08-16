c7fa06cfea8b58dbb705f1a4f48822176ab13206
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-08-16 19:41:58 -0300

    project finish
    just need to instal linters

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const contact = document.getElementById("contact");
 const title = document.querySelector(".title");
 const author = document.querySelector(".author");
 const addButton = document.querySelector(".addBtn");
-/* console.log(title.value); **************************** */
 
 // Sections:
 const main = document.querySelector(".main-container");
 const add = document.querySelector(".add-section");
 const info = document.querySelector(".contact-info");
+
+// Delete button
+const deleteBtn = document.querySelector("#book-list");
 
 /* Event: click "List" in the nav bar */
 // Add a new book:
@@ -32,6 +34,7 @@ navContainer.addEventListener("click", (e) => {
     add.classList.add("hide");
     info.classList.add("hide");
   }
+
   if (e.target.textContent === "Add new") {
     //Active class changing every click
     list.classList.add("active");
@@ -42,6 +45,7 @@ navContainer.addEventListener("click", (e) => {
     add.classList.remove("hide");
     info.classList.add("hide");
   }
+
   if (e.target.textContent === "Contact") {
     //Active class changing every click
     list.classList.add("active");
@@ -58,8 +62,57 @@ navContainer.addEventListener("click", (e) => {
 // document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 addButton.addEventListener("click", () => {
-  if (title.value !== '' && author.value !== '' ) {
+  if (title.value !== "" && author.value !== "") {
     Method.addStorage(title, author);
     Method.addBook(title, author);
   }
-}); 
+});
+
+window.addEventListener('load', () => { 
+  //load from localStorage
+  let storagedBooks = localStorage.getItem('data')? JSON.parse(localStorage.getItem('data')): [];
+  for (let i = 0; i < storagedBooks.length; i += 1) {
+    //create book container
+    const div1 = document.createElement('div');
+    div1.classList.add('div1');
+  
+    //create p to title and author
+    const p = document.createElement('p');
+    p.classList.add('text-book');
+    p.textContent = `${storagedBooks[i].title} by ${storagedBooks[i].author}`;
+  
+    // add p to div1
+    div1.appendChild(p);
+  
+    // delete button and appended to div1
+    const deleted = document.createElement('button')
+    deleted.classList.add('delete');
+    deleted.textContent = 'Delete 🗑️';
+    div1.appendChild(deleted);
+  
+    // Conteiner for all books
+    const bookList = document.querySelector('.book-list');	
+    bookList.appendChild(div1); //Book and button added to a container!
+  }
+  storagedBooks = [];
+});
+
+deleteBtn.addEventListener("click", (event) => {
+  if (event.target.textContent === 'Delete 🗑️') {
+    
+    //erase the parent of the delete button checking its content.
+    const byeObject = event.target.previousSibling.textContent;
+    event.target.parentElement.remove();
+
+    /* erase the object in localStorage. */
+    // split string of x and use the 1st word
+    const objectArray = byeObject.split(' ');
+    console.log(objectArray)
+    //LOAD the local Storage and filter the erased object in a new const.
+    let books = JSON.parse(localStorage.getItem('data'));
+    console.log(books)
+		const erasedBook = books.filter(x => x.title !== objectArray[0]);
+    console.log(erasedBook)
+		localStorage.setItem('data', JSON.stringify(erasedBook));
+  }
+});
--- modules/methods.js
+++ modules/methods.js
@@ -1,38 +1,52 @@
 import Book from "./constructor.js";
 
+let id = 0;
 export default class Method {
 	static addStorage(title, author) {
-		const storage = new Book(title.value, author.value);
-		let books = [];
-		books += books.push(storage);
+		const storage = new Book(`"${title.value}"`, author.value);
+		let books = localStorage.getItem('data')? JSON.parse(localStorage.getItem('data')): [];
+		books.push(storage)
 		localStorage.setItem('data', JSON.stringify(books));
 
 
-		console.log(storage);
+		console.log(books);
 	}
 
   static addBook(title, author) {
     const book = new Book(title.value, author.value);
+
 		//create book container
 		const div1 = document.createElement('div');
 		div1.classList.add('div1');
+		div1.id = `${id}` // id to use for delete funtion.
+
 		//create p to title and author
 		const p = document.createElement('p');
 		p.classList.add('text-book');
-		p.textContent = `"${book.title}" by ${book.author}`
+		p.textContent = `"${book.title}" by ${book.author}`;
+
 		// add p to div2
 		div1.appendChild(p);
 		// add div1 to list for all books
+
 		// delete button and appended to div1
 		const deleted = document.createElement('button')
 		deleted.classList.add('delete');
 		deleted.textContent = 'Delete 🗑️';
 		div1.appendChild(deleted);
+
 		// Conteiner for all books
 		const bookList = document.querySelector('.book-list');	
 		bookList.appendChild(div1); //Book and button added!
+
 		//clear inputs
 		title.value = '';
 		author.value = '';
+		id++;
   }
+
+	/* static deleteButton(e) {
+		e.target.parentElement.remove();
+	} */
 }
+
